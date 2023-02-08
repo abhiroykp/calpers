e5cf3b464b958e0ad02e7a9731a1aced62b4957d
Abhishek Roy <abhishek.x1.roy@kp.org>
2023-02-08 22:38:10 +0530

    calpers footer added with responsive design

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,30 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function wrapElements() {
+  setTimeout(() => {
+    let ele  = document.querySelector('.custom-footer > div').children;
+
+    // element that will be wrapped
+    var el = ele[0];
+
+    // create wrapper container
+    var wrapper = document.createElement('div')
+    wrapper.classList.add('footer-logo');
+
+    // insert wrapper before el in the DOM tree
+    el.parentNode.insertBefore(wrapper, el);
+
+    // move el into wrapper
+    wrapper.appendChild(el);
+
+
+    // wrap all the rest p tags inside a div using jquery
+    let ele1  = $( ".custom-footer > div > p" ).wrapAll( "<div class='footer-desc' />");
+
+  }, 1000);
+
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
@@ -13,7 +38,9 @@ export default async function decorate(block) {
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
   const footer = document.createElement('div');
+  footer.classList.add('custom-footer');
   footer.innerHTML = html;
   await decorateIcons(footer);
+  wrapElements();
   block.append(footer);
 }
